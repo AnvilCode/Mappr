17a4ad67a34f44a667e2d1c0221e91402c66eccc
sohaildani <mohdsohaildk@gmail.com>
2016-08-06 19:16:46 +0530

    Admin Activity Completed

--- app/src/main/java/com/anvil/mappr/AdminActivity.java
+++ app/src/main/java/com/anvil/mappr/AdminActivity.java
@@ -1,17 +1,26 @@
 package com.anvil.mappr;
 
+import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
 
 
 /**
  * Created by RAJEEV YADAV on 8/6/2016.
  */
 public class AdminActivity extends AppCompatActivity{
-
+  ListView list;
+    String[] AdminActivities;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,5 +28,33 @@ public class AdminActivity extends AppCompatActivity{
         Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
         toolbar.setTitle("Mappr");
+        list= (ListView) findViewById(R.id.list_admin);
+        Resources res=getResources();
+        AdminActivities=res.getStringArray(R.array.AdminList);
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,AdminActivities);
+        list.setAdapter(adapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch(position){
+                    case 1:
+                        Intent intt1=new Intent(this,CreatePoll.class);
+                        startActivity(intt1);
+                        break;
+                    case 2:
+                        Intent intt2=new Intent(this,Attendence.class);
+                        startActivity(intt2);
+                        break;
+                    case 3:
+                        Intent intt3=new Intent(this,StudentMakes.class);
+                        startActivity(intt3);
+                        break;
+                    case 4:
+                        Intent intt4=new Intent(this,StudentFees.class);
+                        startActivity(intt4);
+                        break;
+                }
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/anvil/mappr/AdminAdapter.java
@@ -0,0 +1,80 @@
+package com.anvil.mappr;
+
+import android.content.Context;
+import android.database.DataSetObserver;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+/**
+ * Created by pc on 06-08-2016.
+ */
+public class AdminAdapter extends ListAdapter{
+
+    AdminAdapter(Context context,View view,String[] AdminActivities){
+
+
+    }
+
+    @Override
+    public boolean areAllItemsEnabled() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled(int position) {
+        return false;
+    }
+
+    @Override
+    public void registerDataSetObserver(DataSetObserver observer) {
+
+    }
+
+    @Override
+    public void unregisterDataSetObserver(DataSetObserver observer) {
+
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return 0;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+}
