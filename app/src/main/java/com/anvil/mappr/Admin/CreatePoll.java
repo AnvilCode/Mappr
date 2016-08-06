66d15cbf20ba4b71a2d70302b0038ffd6409503f
sohaildani <mohdsohaildk@gmail.com>
2016-08-07 01:25:54 +0530

    create poll made

--- app/src/main/java/com/anvil/mappr/Admin/CreatePoll.java
+++ app/src/main/java/com/anvil/mappr/Admin/CreatePoll.java
@@ -1,11 +1,19 @@
 package com.anvil.mappr.Admin;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+
+import com.anvil.mappr.R;
 
 /**
  * Created by RAJEEV YADAV on 8/6/2016.
  */
 public class CreatePoll extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_poll);
 
-
+    }
 }
